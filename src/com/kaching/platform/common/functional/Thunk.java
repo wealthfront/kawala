986af4aa9338f8bd7ab2301c1c6745a95968960a
jaredjacobs <jaredjacobs@d744d98c-a4b2-11de-97f0-97815e7b2756>
2010-05-07 16:18:07 +0000

    clarifying Thunk and eliminating an unnecessary call
    git-svn-id: https://kawala.googlecode.com/svn/trunk@102 d744d98c-a4b2-11de-97f0-97815e7b2756

--- src/com/kaching/platform/common/functional/Thunk.java
+++ src/com/kaching/platform/common/functional/Thunk.java
@@ -1,35 +1,29 @@
 package com.kaching.platform.common.functional;
 
 /**
- * A {@link Thunk} represents a lazy computation.
- *
- * No conditional logic in this super fast implementation!
+ * A lazy computation. No conditional logic in this super fast implementation!
  */
 public abstract class Thunk<T> {
 
-  private volatile Getter<T> getter = new Getter<T>() {
-      Getter<T> synchedGetter = new Getter<T>() {
-        @Override
+  private volatile Getter<T> threadsafeGetter = new Getter<T>() {
+      Getter<T> getter = new Getter<T>() {
         T get() {
           final T value = compute();
-          getter = new Getter<T>() {
-            @Override
+          threadsafeGetter = getter = new Getter<T>() {
             T get() {
               return value;
             }
           };
-          synchedGetter = getter;
-          return getter.get();
+          return value;
         }
       };
-      @Override
       synchronized T get() {
-        return synchedGetter.get();
+        return getter.get();
       }
     };
 
   public final T get() {
-    return getter.get();
+    return threadsafeGetter.get();
   }
 
   abstract protected T compute();
