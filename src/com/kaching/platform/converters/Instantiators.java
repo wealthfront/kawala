44d9eff544492caf669b2a8633d56c9321e33d38
pascallouisperez <pascallouisperez@d744d98c-a4b2-11de-97f0-97815e7b2756>
2010-05-18 06:01:24 +0000

    Starting on converter creation logic.
    git-svn-id: http://kawala.googlecode.com/svn/trunk@128 d744d98c-a4b2-11de-97f0-97815e7b2756

--- src/com/kaching/platform/converters/Instantiators.java
+++ src/com/kaching/platform/converters/Instantiators.java
@@ -10,7 +10,9 @@
  */
 package com.kaching.platform.converters;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Type;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -26,16 +28,56 @@ public class Instantiators {
     Constructor<T> constructor = getConstructor(klass);
     constructor.setAccessible(true);
     // 2. for each parameter, find converter
-    int parametersCount = constructor.getParameterTypes().length;
+    Type[] genericParameterTypes = constructor.getGenericParameterTypes();
+    Annotation[][] parameterAnnotations = constructor.getParameterAnnotations();
+    int parametersCount = genericParameterTypes.length;
     Converter<?>[] converters =
         parametersCount == 0 ? null : new Converter<?>[parametersCount];
     for (int i = 0; i < parametersCount; i++) {
-      converters[i] = null; // do something smart here
+      converters[i] = createConverter(
+          genericParameterTypes[i], parameterAnnotations[i]);
     }
     // 3. done
     return new InstantiatorImpl<T>(constructor, converters);
   }
   
+  @VisibleForTesting
+  @SuppressWarnings("unchecked")
+  static Converter<?> createConverter(
+      Type type, Annotation[] annotations) {
+    try {
+      // 1. explicit binding
+      if (type instanceof Class) {
+        // 2. @ConvertedBy
+        Annotation[] typeAnnotations = ((Class) type).getAnnotations();
+        // need to throw if two @ConvertedBy!!!
+        for (Annotation typeAnnotation : typeAnnotations) {
+          if (typeAnnotation instanceof ConvertedBy) {
+            return ((ConvertedBy) typeAnnotation).value().newInstance();
+          }
+        }
+        // 3. has <init>(Ljava/lang/String;)V;
+        Constructor stringConstructor = ((Class) type).getConstructor(String.class);
+        stringConstructor.setAccessible(true);
+        return null;//new ConstructorAndToStringConverter<?>(stringConstructor);
+      }
+      return null; // TODO(pascal): should accumulate error (i.e. binding error,
+      // cannot create converter for type XYZ)
+    } catch (InstantiationException e) {
+      // proper error handling
+      throw new RuntimeException(e);
+    } catch (IllegalAccessException e) {
+      // proper error handling
+      throw new RuntimeException(e);
+    } catch (SecurityException e) {
+      // proper error handling
+      throw new RuntimeException(e);
+    } catch (NoSuchMethodException e) {
+      // proper error handling
+      throw new RuntimeException(e);
+    }
+  }
+
   @VisibleForTesting
   static <T> Constructor<T> getConstructor(Class<T> clazz) {
     @SuppressWarnings("unchecked")
