645c39d2ab8056a0cf7b08ba95256ec50c6abdb8
pascallouisperez <pascallouisperez@d744d98c-a4b2-11de-97f0-97815e7b2756>
2010-05-22 00:49:13 +0000

    Highlighting simple uses cases of the library.
    git-svn-id: http://kawala.googlecode.com/svn/trunk@157 d744d98c-a4b2-11de-97f0-97815e7b2756

--- srctest/com/kaching/platform/converters/InstantiatorsTest.java
+++ srctest/com/kaching/platform/converters/InstantiatorsTest.java
@@ -10,8 +10,96 @@
  */
 package com.kaching.platform.converters;
 
+import static java.lang.String.format;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class InstantiatorsTest {
 
-  // empty, for now
-  
+  @Test
+  public void constructMe1() {
+    assertNotNull(Instantiators
+        .createInstantiator(ConstructMe1.class)
+        .newInstance());
+
+  }
+
+  @Test
+  public void constructMe2() {
+    ConstructMe2 instance = Instantiators
+        .createInstantiator(ConstructMe2.class)
+        .newInstance("Jack Bauer");
+    assertNotNull(instance);
+    assertEquals("Jack Bauer", instance.name);
+  }
+
+  @Test
+  public void constructMe3() {
+    ConstructMe3 instance = Instantiators
+        .createInstantiator(ConstructMe3.class)
+        .newInstance("Jack Bauer", "First:Last");
+    assertNotNull(instance);
+    assertEquals("Jack Bauer", instance.name.content);
+    assertEquals("First", instance.pair.first);
+    assertEquals("Last", instance.pair.last);
+  }
+
+  static class ConstructMe1 {
+    ConstructMe1() {}
+  }
+
+  static class ConstructMe2 {
+    private final String name;
+    ConstructMe2(String name) {
+      this.name = name;
+    }
+  }
+
+  static class ConstructMe3 {
+    private final WrappedString name;
+    private final ConvertedPair pair;
+    ConstructMe3(WrappedString name, ConvertedPair pair) {
+      this.name = name;
+      this.pair = pair;
+    }
+  }
+
+  static class WrappedString {
+    private final String content;
+    WrappedString(String content) {
+      this.content = content;
+    }
+    @Override
+    public String toString() {
+      return content;
+    }
+  }
+
+  @ConvertedBy(ConvertedPairConverter.class)
+  static class ConvertedPair {
+    private final String first;
+    private final String last;
+    ConvertedPair(String first, String last) {
+      this.first = first;
+      this.last = last;
+    }
+  }
+
+  static class ConvertedPairConverter implements Converter<ConvertedPair> {
+
+    @Override
+    public String toString(ConvertedPair value) {
+      return format("%s:%s", value.first, value.last);
+    }
+
+    @Override
+    public ConvertedPair fromString(String representation) {
+      String[] parts = representation.split(":");
+      return new ConvertedPair(parts[0], parts[1]);
+    }
+
+  }
+
 }
