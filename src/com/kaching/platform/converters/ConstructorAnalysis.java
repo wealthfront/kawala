350a2ee4f4866c0475294c85dff6f88396c6a431
pascallouisperez <pascallouisperez@d744d98c-a4b2-11de-97f0-97815e7b2756>
2010-05-27 23:27:29 +0000

    Starting the constructor analysis to automatically infer field to parameter assignment. Adding dependency between converters and asm & asm-commons.
    git-svn-id: https://kawala.googlecode.com/svn/trunk@165 d744d98c-a4b2-11de-97f0-97815e7b2756

---
+++ src/com/kaching/platform/converters/ConstructorAnalysis.java
@@ -0,0 +1,358 @@
+package com.kaching.platform.converters;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.newHashMap;
+import static java.lang.String.format;
+import static org.apache.commons.logging.LogFactory.getLog;
+import static org.objectweb.asm.ClassReader.SKIP_DEBUG;
+import static org.objectweb.asm.ClassReader.SKIP_FRAMES;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import org.apache.commons.logging.Log;
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.Attribute;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.EmptyVisitor;
+
+import com.google.common.base.Joiner;
+
+public class ConstructorAnalysis {
+
+  private static final Log log = getLog(ConstructorAnalysis.class);
+
+  /**
+   * Produces an assignment or field names to values or fails.
+   * @throws IllegalConstructorException
+   */
+  static Map<String, JavaValue> analyse(
+      Class<?> klass, Constructor<?> constructor) throws IOException {
+    final String constructorDescriptor =
+        Type.getConstructorDescriptor(constructor);
+    final ConstructorExecutionState state =
+        new ConstructorExecutionState(
+            klass,
+            constructor.getParameterTypes().length);
+    final boolean[] hasVisitedConstructor = new boolean[] { false };
+    analyse(klass, new EmptyVisitor() {
+      @Override
+      public MethodVisitor visitMethod(int access, String name, String desc,
+          String signature, String[] exceptions) {
+        if (name.equals("<init>") &&
+            desc.equals(constructorDescriptor)) {
+          if (!hasVisitedConstructor[0]) {
+            hasVisitedConstructor[0] = true;
+            return new ConstructorAnalyzer(state);
+          } else {
+            throw new IllegalStateException();
+          }
+        } else {
+          return this;
+        }
+      }
+    });
+    return state.assignements;
+  }
+
+  /**
+   * This constructor analyzer does abstract interpretation of the code
+   * of a constructor to:
+   * 1. Determine whether this is a legal kawala constructor
+   *    (per the instantiator spec)
+   * 2. Infer the parameter index of each field
+   * @see http://homepages.inf.ed.ac.uk/kwxm/JVM/codeByNo.html for quick
+   *    reference on opcodes
+   */
+  static class ConstructorAnalyzer implements MethodVisitor {
+
+    private final ConstructorExecutionState state;
+
+    private ConstructorAnalyzer(ConstructorExecutionState state) {
+      this.state = state;
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+      return new EmptyVisitor();
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotationDefault() {
+      return new EmptyVisitor();
+    }
+
+    @Override
+    public void visitAttribute(Attribute attr) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitCode() {
+    }
+
+    @Override
+    public void visitEnd() {
+      log.trace(format("end state:\n%s", state));
+      checkState(state.stack.isEmpty(), "stack not empty on exit");
+    }
+
+    @Override
+    public void visitFieldInsn(int opcode, String owner, String name,
+        String desc) {
+      switch (opcode) {
+        case 0xB5: // putfield
+          log.trace(format("putfield %s %s %s", owner, name, desc));
+          JavaValue value = state.stack.pop();
+          state.stack.pop();
+          state.assign(name, value);
+          return;
+
+        default: unknown(opcode);
+      }
+    }
+
+    @Override
+    public void visitFrame(int type, int nLocal, Object[] local, int nStack, Object[] stack) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitIincInsn(int var, int increment) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitInsn(int opcode) {
+      switch (opcode) {
+        case 0xB1: // return
+          log.trace("return");
+          return;
+
+        case 0x03: // iconst_0
+        case 0x04: // iconst_1
+        case 0x05: // iconst_2
+        case 0x06: // iconst_3
+        case 0x07: // iconst_4
+        case 0x08: // iconst_5
+          int n = opcode - 0x3;
+          log.trace(format("iconst_%s", n));
+          state.stack.push(new JavaConstant(n));
+          return;
+
+        default: unknown(opcode);
+      }
+    }
+
+    @Override
+    public void visitIntInsn(int opcode, int operand) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitJumpInsn(int opcode, Label label) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitLabel(Label label) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitLdcInsn(Object cst) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitLineNumber(int line, Label start) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitLocalVariable(String name, String desc, String signature,
+        Label start, Label end, int index) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitMaxs(int maxStack, int maxLocals) {
+    }
+
+    @Override
+    public void visitMethodInsn(
+        int opcode, String owner, String name, String desc) {
+      switch (opcode) {
+        case 0xB7: // invokespecial
+          log.trace(format("invokespecial %s %s %s", owner, name, desc));
+          if (!owner.equals(state.owner) &&
+              name.equals("<init>") &&
+              desc.equals("()V")) {
+            /* super()
+             * consumes object reference
+             */
+            state.stack.pop();
+            break;
+          }
+          if (owner.equals(state.owner) &&
+              name.equals("<init>") &&
+              desc.equals("()V")) {
+            throw new IllegalConstructorException(
+                "cannot call delegate constructor");
+          }
+          throw new IllegalStateException();
+
+        default: unknown(opcode);
+      }
+    }
+
+    @Override
+    public void visitMultiANewArrayInsn(String desc, int dims) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AnnotationVisitor visitParameterAnnotation(int parameter,
+        String desc, boolean visible) {
+      // TODO we need to capture @Optional parameters
+      return new EmptyVisitor();
+    }
+
+    @Override
+    public void visitTableSwitchInsn(int min, int max, Label dflt,
+        Label[] labels) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitTryCatchBlock(Label start, Label end, Label handler,
+        String type) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitTypeInsn(int opcode, String desc) {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void visitVarInsn(int opcode, int var) {
+      switch (opcode) {
+        case 0x15: // iload
+        case 0x19: // aload
+          log.trace(format("_load %s", var));
+          state.stack.push(state.locals.get(var));
+          return;
+
+        default: unknown(opcode);
+      }
+    }
+
+    private void unknown(int opcode) {
+      throw new IllegalStateException(format("unknown opcode %s", opcode));
+    }
+
+  }
+
+  /**
+   * This constructor execution state captures the abstract interpretation of
+   * a constructor's instructions.
+   */
+  static class ConstructorExecutionState {
+
+    private final String owner;
+    private final List<LocalVariable> locals = newArrayList();
+    private final Stack<JavaValue> stack = new Stack<JavaValue>();
+    private final Map<String, JavaValue> assignements = newHashMap();
+
+    ConstructorExecutionState(Class<?> klass, int argumentsNum) {
+      owner = klass.getName().replace('.', '/');
+      locals.add(new LocalVariable("this"));
+      for (int i = 1; i <= argumentsNum; i++) {
+        locals.add(new LocalVariable(format("p%s", i - 1)));
+      }
+    }
+
+    void assign(String field, JavaValue value) {
+      if (assignements.containsKey(field)) {
+        throw new IllegalConstructorException("duplicate assignment to field %s", field);
+      } else {
+        assignements.put(field, value);
+      }
+    }
+
+    @Override
+    public String toString() {
+      return Joiner.on("\n").join(
+          format("locals: %s", locals), format("stack : %s", stack),
+          format("assign: %s", assignements));
+    }
+
+  }
+
+  static class IllegalConstructorException extends RuntimeException {
+
+    private static final long serialVersionUID = -7666362130696013958L;
+
+    IllegalConstructorException(String format, Object... args) {
+      super(format(format, args));
+    }
+
+  }
+
+  interface JavaValue {
+  }
+
+  static class LocalVariable implements JavaValue {
+    private final String name;
+    public LocalVariable(String name) {
+      this.name = name;
+    }
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
+  static class JavaConstant implements JavaValue {
+    private final Object value;
+    public JavaConstant(Object value) {
+      this.value = value;
+    }
+    @Override
+    public String toString() {
+      return value.toString();
+    }
+  }
+
+  private static void analyse(Class<?> klass, ClassVisitor visitor) throws IOException {
+    InputStream in = null;
+    try {
+      in = klass.getResourceAsStream("/" + klass.getName().replace('.', '/') + ".class");
+      if (in == null) {
+        throw new IllegalArgumentException(format("cannot find bytecode for %s", klass));
+      }
+      ClassReader reader = new ClassReader(in);
+      reader.accept(visitor, SKIP_FRAMES | SKIP_DEBUG);
+    } finally {
+      if (in != null) {
+        in.close();
+      }
+    }
+  }
+
+}
---
+++ srctest/com/kaching/platform/converters/ConstructorAnalysisTest.java
@@ -0,0 +1,102 @@
+package com.kaching.platform.converters;
+
+import static com.google.common.collect.Maps.newHashMap;
+import static java.util.Collections.emptyMap;
+import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+import com.kaching.platform.converters.ConstructorAnalysis.IllegalConstructorException;
+import com.kaching.platform.converters.ConstructorAnalysis.JavaValue;
+
+public class ConstructorAnalysisTest {
+
+  static class NoOp {
+  }
+
+  @Test
+  public void noOp() throws Exception {
+    assertAssignement(NoOp.class, emptyMap());
+  }
+
+  static class OneAssignment {
+    final int foo;
+    OneAssignment(int foo) {
+      this.foo = foo;
+    }
+  }
+
+  @Test
+  public void oneAssignment() throws Exception {
+    assertAssignement(
+        OneAssignment.class,
+        ImmutableMap.of(
+            "foo", "p0"));
+  }
+
+  static class TwoAssignments {
+    final int foo;
+    final int bar;
+    TwoAssignments(int bar, int foo) {
+      this.bar = bar;
+      this.foo = foo;
+    }
+  }
+
+  @Test
+  public void twoAssignments() throws Exception {
+    assertAssignement(
+        TwoAssignments.class,
+        ImmutableMap.of(
+            "bar", "p0",
+            "foo", "p1"));
+  }
+
+  static class DupAssignment {
+    int foo;
+    DupAssignment(int bar, int foo) {
+      this.foo = bar;
+      this.foo = foo;
+    }
+  }
+
+  @Test
+  public void dupAssignment() throws Exception {
+    assertAnalysisFails(
+        DupAssignment.class, "duplicate assignment to field foo");
+  }
+
+  private void assertAnalysisFails(Class<?> klass, String message) throws IOException {
+    try {
+      ConstructorAnalysis.analyse(klass, klass.getDeclaredConstructors()[0]);
+      fail();
+    } catch (IllegalConstructorException e) {
+      assertEquals(message, e.getMessage());
+    }
+  }
+
+  private void assertAssignement(
+      Class<?> klass, Map<?, ?> expected) throws IOException {
+    Map<String, JavaValue> assignements =
+        ConstructorAnalysis.analyse(klass, klass.getDeclaredConstructors()[0]);
+    Map<String, String> actual = newHashMap();
+    for (Entry<String, JavaValue> entry : assignements.entrySet()) {
+      actual.put(entry.getKey(), entry.getValue().toString());
+    }
+    assertEquals(expected, actual);
+  }
+
+  /*private <Q extends Query<?>> void assertError(
+      Class<Q> queryClass, String... errors) throws IOException {
+    ConstructorExecutionState state = analyse(queryClass);
+    assertEquals(newArrayList(errors), state.getErrors());
+  }*/
+
+
+}
