1f764bdd5ec3b67fb3054dbe24484298d1f5a085
vpatryshev <vpatryshev@d744d98c-a4b2-11de-97f0-97815e7b2756>
2010-07-07 03:57:01 +0000

    Added test cases for Comparables' comparison.
    git-svn-id: http://kawala.googlecode.com/svn/trunk@234 d744d98c-a4b2-11de-97f0-97815e7b2756

--- srctesting/com/kaching/platform/testing/EquivalenceTesterTest.java
+++ srctesting/com/kaching/platform/testing/EquivalenceTesterTest.java
@@ -12,6 +12,7 @@ package com.kaching.platform.testing;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -21,8 +22,7 @@ public class EquivalenceTesterTest {
   public void nothinCanBeEqualToNull() {
     EquivalenceTester.check(newArrayList(new EqualsDoesNotHandleNullArg()));
   }
-  
-  
+
   static class EqualsDoesNotHandleNullArg {
     @Override
     public boolean equals(Object that) {
@@ -30,5 +30,55 @@ public class EquivalenceTesterTest {
       return true;
     }
   }
-  
+
+  @Test
+  public void comparisonCompliance() {
+    EquivalenceTester.check(
+        newArrayList(weird(0, 2), weird(0, 12)),
+        newArrayList(weird(0, 5), weird(0, 15)));
+  }
+
+  @Test
+  public void comparisonCompliance_wontReturn0() {
+    try {
+      EquivalenceTester.check(
+          newArrayList(weird(1, 2), weird(2, 12)),
+          newArrayList(weird(2, 5), weird(0, 15)));
+      fail("Comparison should have returned non-zero");
+    } catch (Throwable t) {
+      // as expected
+    }
+  }
+
+  WeirdClass weird(int shift, int value) {
+    return new WeirdClass(shift, value);
+  }
+
+  static class WeirdClass implements Comparable<WeirdClass> {
+    private int shift;
+    private int value;
+
+    WeirdClass(int shift, int value) {
+      this.shift = shift;
+      this.value = value;
+    }
+
+    @Override
+    public int compareTo(WeirdClass other) {
+      return (shift + value) % 10 - (other.shift + other.value) % 10;
+    }
+
+    public boolean equals(Object o) {
+      return o instanceof WeirdClass
+          && (value - ((WeirdClass) o).value) % 10 == 0;
+    }
+
+    public int hashCode() {
+      return (((shift + value) % 10) + 3);
+    }
+
+    public String toString() {
+      return "WeirdClass(" + shift + "," + value + ")";
+    }
+  }
 }
