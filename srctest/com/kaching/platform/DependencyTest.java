59f70323e5e415ba8f98c5384ca94776454359de
vpatryshev <vpatryshev@d744d98c-a4b2-11de-97f0-97815e7b2756>
2010-07-08 03:53:15 +0000

    Added several classes to platform/hibernate.
    git-svn-id: https://kawala.googlecode.com/svn/trunk@236 d744d98c-a4b2-11de-97f0-97815e7b2756

--- srctest/com/kaching/platform/DependencyTest.java
+++ srctest/com/kaching/platform/DependencyTest.java
@@ -40,6 +40,9 @@ import com.kaching.platform.testing.DependencyTestRunner.Dependencies;
           "com.google.inject.*",
           "com.kaching.platform.common.types"
       }),
+      @CheckPackage(name = "com.kaching.platform.hibernate", mayDependOn = {
+          "com.kaching.platform.common"
+      }),
       @CheckPackage(name = "com.kaching.platform.hibernate.types", mayDependOn = {
           "org.hibernate.*",
           "org.apache.commons.logging", // TODO(adam): remove
