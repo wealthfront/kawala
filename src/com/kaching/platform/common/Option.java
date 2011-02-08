2979130a871088feeaf12900c7fd44293349c0a8
adam.rosien <adam.rosien@d744d98c-a4b2-11de-97f0-97815e7b2756>
2011-02-08 07:11:41 +0000

    Revert "Option.transform() returns Option.some(null) if transform provides null."
    git-svn-id: http://kawala.googlecode.com/svn/trunk@308 d744d98c-a4b2-11de-97f0-97815e7b2756

--- src/com/kaching/platform/common/Option.java
+++ src/com/kaching/platform/common/Option.java
@@ -166,7 +166,7 @@ public abstract class Option<T> implements Iterable<T> {
 
     @Override
     public <V> Option<V> transform(Function<U, V> function) {
-      return Option.some(function.apply(u));
+      return Option.of(function.apply(u));
     }
 
     @Override
