f8833949e4b019f159c3bd40cc19fc1943a62f49
dfortunato@gmail.com <dfortunato@gmail.com@d744d98c-a4b2-11de-97f0-97815e7b2756>
2011-06-01 20:57:29 +0000

    add Strings class to prevent logging from blowing up if the format string doesn't match the arguments
    git-svn-id: https://kawala.googlecode.com/svn/trunk@315 d744d98c-a4b2-11de-97f0-97815e7b2756

---
+++ src/com/kaching/platform/common/Strings.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2010 Wealthfront Inc. Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package com.kaching.platform.common;
+
+public class Strings {
+
+  /**
+   * From {@link com.google.common.base.Preconditions#format} but made public
+   */
+  public static String format(String template, Object... args) {
+    template = String.valueOf(template); // null -> "null"
+
+    // start substituting the arguments into the '%s' placeholders
+    StringBuilder builder = new StringBuilder(
+        template.length() + 16 * args.length);
+    int templateStart = 0;
+    int i = 0;
+    while (i < args.length) {
+      int placeholderStart = template.indexOf("%s", templateStart);
+      if (placeholderStart == -1) {
+        break;
+      }
+      builder.append(template.substring(templateStart, placeholderStart));
+      builder.append(args[i++]);
+      templateStart = placeholderStart + 2;
+    }
+    builder.append(template.substring(templateStart));
+
+    // if we run out of placeholders, append the extra args in square braces
+    if (i < args.length) {
+      builder.append(" [");
+      builder.append(args[i++]);
+      while (i < args.length) {
+        builder.append(", ");
+        builder.append(args[i++]);
+      }
+      builder.append("]");
+    }
+
+    return builder.toString();
+  }
+
+}
